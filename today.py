5882a2e6881a423083e9d8027ec2853c463e2406
emrekayaa <suleymanemrekaya.1@gmail.com>
2025-12-16 23:48:51 +0300

    Fix style issues, should get a 'Good Style' now :pray:

--- today.py
+++ today.py
@@ -1,4 +1,11 @@
+"""A module computing buddy pair names for the day"""
+
 def my_name_is():
+    
     return "emrekayaa"
+"""Return my GitHub username."""
+
+
 def my_buddy_is():
     return "emrekayaa"
+"""Return my buddy's GitHub username."""
